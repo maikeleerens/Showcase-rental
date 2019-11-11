c31e25bd65b90eb86dda09d730df893813e84f05
maikeleerens <maikel@chaincode.eu>
2019-11-11 22:37:48 +0100

    Added swagger 2 ui for api documentation and testing

--- Applicatie/rental/src/main/java/com/rental/Api/Application.java
+++ Applicatie/rental/src/main/java/com/rental/Api/Application.java
@@ -3,8 +3,15 @@ package com.rental.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan({"com.rental.domain.services", "com.rental.Api.controllers"})
@@ -16,4 +23,15 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-}
+    @Configuration
+    @EnableSwagger2
+    public class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("com.rental.Api.controllers"))
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+}}
